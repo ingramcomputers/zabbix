7b6c5ac46588201ff644f3ceaa3cee3cd6213ad6
ingramcomputers <79795957+ingramcomputers@users.noreply.github.com>
2024-03-21 13:34:14 -0500

    erwaref

--- CPU Temp/cputemp_install.ps1
+++ CPU Temp/cputemp_install.ps1
@@ -15,6 +15,20 @@ $parms = "e C:\IT-Support\LibreHardwareMonitorCLI.7z -oC:\IT-Support\bin"
 $Prms = $Parms.Split( )
 & "$command" $Prms
 
+# Wait for things to complete because powershell can be a bitch
 Start-Sleep 15
-
+# Delete the file to keep thing tidy
 Remove-Item C:\IT-Support\LibreHardwareMonitorCLI.7z
+
+# Download the script to run and send temp to Zabbix
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ingramcomputers/zabbix/main/CPU%20Temp/cputemp.ps1", "C:\IT-Support\bin\cputemp.ps1")
+
+# Create the scheduled task that runs every 5 minutes
+$Trigger = New-ScheduledTaskTrigger -Daily -At '00:00'
+$Action = New-ScheduledTaskAction -Execute "PowerShell" -Argument "C:\IT-Support\bin\cputemp.ps1"
+$Principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
+$task = Register-ScheduledTask -TaskName "Zabbix CPU Temperature" -Trigger $Trigger -Action $Action -Principal $Principal
+$task.Triggers.Repetition.Duration = 'P7300D'
+$task.Triggers.Repetition.Interval = 'PT5M'
+$task | Set-ScheduledTask
+
